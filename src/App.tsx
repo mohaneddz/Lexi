445decffca550bdb0bd7ada40b16ccc83303ab07
Mohaned <akumaxd5@gmail.com>
2026-02-10 10:57:48 +0100

    chore: separate logical implementation group

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import Titlebar from '@/layout/Titlebar';
-
-import Home from '@/routes/Home';
+import { useTheme } from "@/hooks/useTheme";
+import { AppShell } from "@/layout/AppShell";
+import Definitions from "@/routes/Definitions";
+import Inbox from "@/routes/Inbox";
+import Review from "@/routes/Review";
+import Settings from "@/routes/Settings";
+import Stats from "@/routes/Stats";
+import Translations from "@/routes/Translations";
+import Words from "@/routes/Words";
 
 function App() {
+  useTheme();
+
   return (
-    <>
-      <Titlebar />
-      <Router>
-        <main className="screen center col overflow-y-auto">
-          <Routes>
-            {/* Main pages */}
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </main>
-      </Router>
-    </>
+    <BrowserRouter>
+      <AppShell>
+        <Routes>
+          <Route path="/" element={<Navigate to="/inbox" replace />} />
+          <Route path="/inbox" element={<Inbox />} />
+          <Route path="/words" element={<Words />} />
+          <Route path="/translations" element={<Translations />} />
+          <Route path="/definitions" element={<Definitions />} />
+          <Route path="/review" element={<Review />} />
+          <Route path="/stats" element={<Stats />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="*" element={<Navigate to="/inbox" replace />} />
+        </Routes>
+      </AppShell>
+    </BrowserRouter>
   );
 }
 
